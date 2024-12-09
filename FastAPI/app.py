f509b38a2a32c2c31ac16dc76cb88d357137a984
Nabil Hasan <nabil.hasan01@northsouth.edu>
2024-12-10 05:52:24 +0600

    leaf disease model train speedup

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -104,4 +104,4 @@ async def predict_soil(input_data: SoilInput):
 # Run the app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
